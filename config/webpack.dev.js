cb2ef09be471922177e42e790041f4332cd8f844
杨坤海 <384037191@qq.com>
2018-07-24 17:07:10 +0800

    add webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports ={
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+     path: path.resolve(__dirname,'../dist' ),
+     filename: '[name].js',
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js|jsx$/,
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              'react',
+            ],
+            plugins: [
+              ['transform-object-rest-spread', { useBuiltIns: true }],
+              'transform-decorators-legacy',
+              'transform-class-properties',
+              'transform-exponentiation-operator',
+            ],
+          },
+        }],
+      },
+      {
+        test: /\.less$/,
+        use:[
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'less-loader'},
+        ]
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader',
+      },
+    ],
+  },
+  plugins: [],
+  devServer:{
+    contentBase: path.resolve(__dirname, '../dist'),
+    host: 'localhost',
+    compress: true,
+    port: 8893,
+  }
+}
